f66033997ba71099af741054fc649a1b8f0ab5fc
Gaboelc <gabrielleon917@gmail.com>
2019-12-29 01:00:33 -0600

    Created install_programs.ps1
    Created update_programs.ps1
    
    Created README.md

---
+++ install_programs.ps1
@@ -0,0 +1,55 @@
+#Programs
+
+#Browsers
+
+#winrar
+Scoop install winrar
+#Google Chrome
+Scoop install googlechrome
+#Firefox
+Scoop install firefox
+#Opera
+Scoop install opera
+
+#Dev tools
+
+#Vs code
+Scoop install vscode
+
+#Arduino ide
+Scoop install arduino
+
+#Node.js
+Scoop install nodejs
+
+#Mongodb
+Scoop install mongodb-compass-community
+
+#git
+Scoop install git
+
+#Anaconda3
+Scoop install anaconda3
+
+#Computing
+
+#WinSCP
+Scoop install winscp
+
+#Gaming
+
+#Steam
+Scoop install steam
+
+#desing
+
+#Inkscape
+Scoop install inkscape
+
+#OBS Studio
+Scoop install obs-studio
+
+#Others
+
+#Telegram
+Scoop install telegram
---
+++ update_programs.ps1
@@ -0,0 +1,50 @@
+#Updating scoop
+scoop update
+
+#Browsers
+
+#winrar
+scoop update winrar
+#Google Chrome
+scoop update googlechrome
+#Firefox
+scoop update firefox
+#Opera
+scoop update opera
+
+#Dev tools
+
+#Vs code
+scoop update vscode
+#Arduino ide
+scoop update arduino
+#Node.js
+scoop update nodejs
+#Mongodb
+scoop update mongodb-compass-community
+#git
+scoop update git
+#Anaconda3
+scoop update anaconda3
+
+#Computing
+
+#WinSCP
+scoop update winscp
+
+#Gaming
+
+#Steam
+scoop update steam
+
+#desing
+
+#Inkscape
+scoop update inkscape
+#OBS Studio
+scoop update obs-studio
+
+#Others
+
+#Telegram
+scoop update telegram
