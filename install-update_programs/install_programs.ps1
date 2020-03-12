414d91cae81edf2d4b43f9279a295fd04eb6f173
unknown <gabrielleon917@gmail.com>
2020-03-11 21:00:23 -0600

    Install_programs.ps1 moddified

--- install-update_programs/install_programs.ps1
+++ install-update_programs/install_programs.ps1
@@ -3,9 +3,13 @@ Set-ExecutionPolicy RemoteSigned -scope CurrentUser
 Write-Output "Execition Policy changed"
 Start-Sleep -s 2
 
-# #Installing Scoop
+#Installing Scoop
 Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
 Write-Output "Scoop Installed"
+Start-Sleep -s 2
+
+#install git to add extra bucket
+Scoop install git
 Start-Sleep -s 2
 
 #Adding extra bucket
@@ -14,13 +18,13 @@ scoop bucket add extras
 Start-Sleep -s 5
 
 #Array of programs
-$programs = "winrar","googlechrome","firefox","opera","vscode","arduino","nodejs","mongodb-compass-community","git","python","anaconda3","notepadplusplus","winscp","steam","inkscape","obs-studio","telegram"
+$programs = "winrar","googlechrome","firefox","opera","vscode","arduino","nodejs","mongodb-compass-community","python","anaconda3","notepadplusplus","r","rstudio","winscp","steam","inkscape","obs-studio","telegram"
 Write-Output  "This script will install", $programs
 
 #Programs installation
 
 for ($i = 0; $i -lt $programs.Count; $i++) {
-    Scoop install $programs[$i]
+    Start-Process powershell.exe /c Scoop install $programs[$i]
     Write-Output $programs[$i] "INSTALLED"
     Start-Sleep -s 2
 }
